d74383943fd2347080535cac62200f2b46286ece
waler5 <walerr@mail.ru>
2017-12-24 12:32:58 +0300

    add main

---
+++ src/com/Main.java
@@ -0,0 +1,13 @@
+package com;
+
+public class Main {
+
+    public static void main(String[] args) {
+/*        new FirstClass().print();
+        new SecondClass().print();
+        new ThirdClass().print();
+        new FourthClass().print();
+        new FifthClass().print();*/
+    }
+
+}
